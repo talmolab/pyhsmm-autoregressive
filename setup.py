2a4c73c08dcda959b9bac2f03a2b976dabbc37af
Ayman Zeine <a.zeine.96@gmail.com>
2020-08-30 21:51:15 -0400

    Updated eigendependency path
    Bumped version number to 0.1.2

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
-from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
 import numpy as np
-from os.path import dirname, join, exists
+from os.path import join, exists
 from os import mkdir
 from shutil import move
 import tarfile
-from six.moves.urllib.request import urlretrieve
+from urllib.request import Request, urlopen
 from glob import glob
 
 # make dependency directory
@@ -14,21 +13,25 @@ if not exists('deps'):
     mkdir('deps')
 
 # download Eigen if we don't have it in deps
-eigenurl = 'http://bitbucket.org/eigen/eigen/get/3.2.6.tar.gz'
+eigenurl = 'https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz'
 eigentarpath = join('deps', 'Eigen.tar.gz')
 eigenpath = join('deps', 'Eigen')
 if not exists(eigenpath):
     print('Downloading Eigen...')
-    urlretrieve(eigenurl, eigentarpath)
+    req = Request(eigenurl, headers={'User-Agent': 'XYZ/3.0'})
+    tar_data = webpage = urlopen(req, timeout=10).read()
+    with open(eigentarpath, 'wb') as f:
+        f.write(tar_data)
+
     with tarfile.open(eigentarpath, 'r') as tar:
         tar.extractall('deps')
-    thedir = glob(join('deps', 'eigen-eigen-*'))[0]
+    thedir = glob(join('deps', 'eigen-*'))[0]
     move(join(thedir, 'Eigen'), eigenpath)
     print('...done!')
 
 setup(
     name='autoregressive',
-    version='0.1.1',
+    version='0.1.2',
     description='Extension for switching vector autoregressive models with pyhsmm',
     author='Matthew James Johnson',
     author_email='mattjj@csail.mit.edu',
